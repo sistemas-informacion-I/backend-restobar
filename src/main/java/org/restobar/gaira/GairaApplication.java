949390533665a3d7d160465e3e5a28a17fc8d53e
SCANDAL9 <duglasrivero2@gmail.com>
2026-04-05 08:51:58 -0400

    main: clase principal de la aplicación

---
+++ src/main/java/org/restobar/gaira/GairaApplication.java
@@ -0,0 +1,13 @@
+package org.restobar.gaira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GairaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GairaApplication.class, args);
+	}
+
+}
