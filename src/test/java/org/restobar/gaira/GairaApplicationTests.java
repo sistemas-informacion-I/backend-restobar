2a948c467a482921b211ede85fcc52ff55432bc7
SCANDAL9 <duglasrivero2@gmail.com>
2026-04-05 08:56:30 -0400

    test: pruebas de la aplicación principal

---
+++ src/test/java/org/restobar/gaira/GairaApplicationTests.java
@@ -0,0 +1,15 @@
+package org.restobar.gaira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class GairaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
